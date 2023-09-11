8e76374a61338f792cb7b1f57e0886e5f117f872
ViktoriiaDoroshenko <Emi90va@gmail.com>
2023-09-11 10:11:43 +0300

    classwork100923

--- src/test/java/tests/RegistrationTests.java
+++ src/test/java/tests/RegistrationTests.java
@@ -8,9 +8,14 @@ public class RegistrationTests extends TestBase {
 
     @Test
     public void registrationPositiveTest(){
+        int i = (int)(System.currentTimeMillis()/1000)%3600;
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("rena" + i + "@gmail.com", "2020Rena!");
+        app.getHelperUser().submitRegistration();
+        app.getHelperUser().pause(3000);
+        Assert.assertTrue(app.getHelperUser().isElementPresent(By.tagName("button")));
         // open login form
         //wd.findElement(By.xpath("//*[.='LOGIN']")).click();
-        app.getHelperUser().openLoginRegistrationForm();
         // fill login form
         //int i = (int)(System.currentTimeMillis()/1000)%3600;
         //WebElement emailInput = wd.findElement(By.xpath("//input[1]"));
@@ -21,15 +26,12 @@ public class RegistrationTests extends TestBase {
         //passInput.click();
         //passInput.clear();
         //passInput.sendKeys("2008Kira!");
-        app.getHelperUser().fillLoginRegistrationForm("rena30@gmail.com", "2020rena!");
         // click on button registration
         //wd.findElement(By.xpath("//button[2]")).click();
-        app.getHelperUser().submitRegistration();
         // assert
         //pause(3000);
-        app.getHelperUser().pause(3000);
         //Assert.assertTrue(wd.findElements(By.tagName("button")).size()>0);
-        Assert.assertTrue(app.getHelperUser().isElementPresent(By.tagName("button")));
+
     }
   @Test
     public void registrationNegativeTestWrongEmail(){
